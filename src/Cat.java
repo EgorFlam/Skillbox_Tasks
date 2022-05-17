ecb97bc568623a61fede016d1066a4272ed15646
Egor FLAM <egorvereschagin@gmail.com>
2022-05-17 22:25:45 +0300

    Create the first backups of completed tasks

---
+++ src/Cat.java
@@ -0,0 +1,88 @@
+public class Cat {
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+
+    private double eatenCount; // кладем сюда колиство съеденной еды
+
+    // 1) добавим статус. можно конечно и enum, но не обязательно, т.к. статус приватный
+    private String status;
+
+    public static int count;
+
+
+    public Cat() {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+        count++;
+
+    }
+
+    public void meow() {
+        weight = weight - 1;
+        System.out.println("Meow");
+    }
+
+    public void feed(Double amount) {
+        weight = weight + amount;
+        eatenCount += amount; // присваиваем коичество съеденной еды к eatenCount
+    }
+
+    public void drink(Double amount) {
+        weight = weight + amount;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    //Урок 2. Метод, параметры, return
+//Создать в классе Cat метод, который будет возвращать массу съеденной еды.
+    public double getEatenCount() {
+        return eatenCount;
+    }
+    //Создать в классе Cat метод «Сходить в туалет» pee(), который уменьшает вес кошки и что-нибудь печатает.
+    public void pee() {
+        weight = weight - weight / 100;
+        System.out.println("Now, I am fine ");
+    }
+
+    public String getStatus() { // прописать условие для статическок переменной,
+        // которая будет увеличиваться, если кошку создали, и убывать, если кошка взорвалась или умерла.
+        if (weight < minWeight) {
+            count--;
+            return "Dead";
+        } else if (weight > maxWeight) {
+            count--;
+            return "Exploded";
+        } else if (weight > originWeight) {
+            return "Sleeping";
+        } else {
+            return "Playing";
+        }
+    }
+    //Урок 3. Статические методы и переменные
+    //Создать у класса Cat статическую переменную count
+    //которая будет увеличиваться, если кошку создали, и убывать, если кошка взорвалась или умерла.
+
+    public static int getCount() {
+        System.out.print("Количество котов: ");
+        return count;
+    }
+
+    //  Урок 5. Научиться создавать объекты, используя перегруженные конструкторы.
+// Что нужно сделать
+//1.	Сделайте в классе Cat ещё один конструктор, чтобы можно было создать кошку с весом, переданным в конструктор.
+//2.	Создайте в главном классе Loader метод генерации котёнка private static Cat getKitten().
+//      Он должен возвращать кошку с весом 1100.00 грамм с помощью конструктора, в который передаём
+//      вес кошки. Используйте описание из видеоурока.
+//3.	Создайте три объекта класса Cat в методе main(), используя метод getKitten().
+
+    public Cat(Double amount) {
+        this.weight = 1100.0;
+    }
+}
---
+++ src/Loader.java
@@ -0,0 +1,97 @@
+public class Loader {
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        System.out.println("Cat1 = " + cat1.getWeight());
+        cat1.feed(10.00);
+        System.out.println("Cat1 (after the feed) = " + cat1.getWeight());
+        System.out.println("Cat1 (after the feed) = " + cat1.getStatus());
+
+        Cat cat2 = new Cat();
+        System.out.println("Cat2 = " + cat2.getWeight());
+        while (cat2.getWeight() >= 1000) {
+            cat2.meow();
+        }
+
+        Cat cat3 = new Cat();
+        System.out.println("Cat3 = " + cat3.getWeight());
+        cat3.feed(30.0);
+        cat3.meow();
+        System.out.println("Cat3 = " + cat3.getWeight());
+        cat3.getEatenCount();
+        System.out.println("Cat3's eatenFood = " + cat3.getEatenCount());
+
+
+        Cat cat4 = new Cat();
+        System.out.println("Cat4 = " + cat4.getWeight());
+        cat4.feed(150.0);
+        cat4.pee();
+        cat4.drink(300.00);
+        cat4.getEatenCount();
+        System.out.println("Cat4 eaten Amount Of Food= " + cat4.getEatenCount());
+        System.out.println("Cat4 = " + cat4.getWeight());
+
+        Cat cat5 = new Cat();
+        System.out.println("Cat5 = " + cat5.getWeight());
+        while (cat5.getWeight() <= 9000) {
+            cat5.drink(1000.0);
+        }
+        System.out.println("===========================================");
+
+        Cat Murka = new Cat();
+        System.out.println("Murka weight: " + Murka.getWeight());
+        Cat Vasya = new Cat();
+        System.out.println("Vasya weight: " + Vasya.getWeight());
+        Cat Misha = new Cat();
+        System.out.println("Misha weight: " + Misha.getWeight());
+        Cat Anton = new Cat();
+        System.out.println("Anton weight: " + Anton.getWeight());
+        Cat Sergey = new Cat();
+        System.out.println("Sergey weight: " + Sergey.getWeight());
+
+        Sergey.pee();
+        while (Sergey.getWeight() > 1000) {
+            Sergey.pee();
+        }
+        Murka.feed(15000.0);
+        Vasya.meow();
+        Anton.drink(100.0);
+        while (Misha.getWeight() > 1000 ) {
+            Misha.meow();
+        }
+        System.out.println("===========================================");
+        System.out.println("Cat1 = " + cat1.getStatus());
+        System.out.println("Cat2 = " + cat2.getStatus());
+        System.out.println("Cat3 = " + cat3.getStatus());
+        System.out.println("Cat4 = " + cat4.getStatus());
+        System.out.println("Cat5 = " + cat5.getStatus());
+        System.out.println("===========================================");
+        System.out.println("Anton 1 = " + Anton.getStatus());
+        System.out.println("Sergey 2 = : " + Sergey.getStatus());
+        System.out.println("Мурка 3 = : " + Murka.getStatus());
+        System.out.println("Misha 4 = :" + Misha.getStatus());
+        System.out.println("Vasya 5 = :" + Vasya.getStatus());
+        System.out.println("===========================================");
+        System.out.println(Cat.getCount());
+        System.out.println("===========================================");
+
+//  Урок 5. Научиться создавать объекты, используя перегруженные конструкторы.
+// Что нужно сделать
+//1.	Сделайте в классе Cat ещё один конструктор, чтобы можно было создать кошку с весом, переданным в конструктор.
+//2.	Создайте в главном классе Loader метод генерации котёнка private static Cat getKitten().
+//      Он должен возвращать кошку с весом 1100.00 грамм с помощью конструктора, в который передаём
+//      вес кошки. Используйте описание из видеоурока.
+//3.	Создайте два объекта класса Cat в методе main(), используя метод getKitten().
+
+        Cat kitten1 = getKitten();
+        System.out.println("kitten1 weight: " + kitten1.getWeight());
+        Cat kitten2 = getKitten();
+        System.out.println("kitten2 weight: " + kitten1.getWeight());
+
+    }
+
+    public static Cat getKitten() { //#2
+        Cat kitten = new Cat(100 + 50 * Math.random());
+        return kitten;
+    }
+}
